a30168f54b045158c9d5fee2072897c420305bd6
sathish <sathishped860@gmail.com>
2024-06-09 20:55:44 +0530

    changing into full column from necessry row and in 4 columns

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+//import 'package:flutter/widgets.dart';
 
 class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({super.key});
@@ -12,24 +12,63 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        children: [
-          const Text("125*20"),
-          const Text("2,500"),
-          Row(
-            children: [
-              Column(
-                children: [
-                  Container(
-                    decoration: const BoxDecoration(color: Colors.green),
-                    child: Text("mc"),
-                  ),
-                ],
-              )
-            ],
-          )
-        ],
+      backgroundColor: const Color.fromARGB(255, 87, 37, 37),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            const SizedBox(
+              height: 170,
+            ),
+            const Text(
+              "125*20",
+              style: TextStyle(
+                  fontSize: 24,
+                  color: Colors.blueGrey,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Text(
+              "2,500",
+              style: TextStyle(
+                  fontSize: 48,
+                  color: Colors.blueGrey,
+                  fontWeight: FontWeight.bold),
+            ),
+            Row(
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(color: Colors.green),
+                      child: const Text("mc"),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(color: Colors.green),
+                      child: const Text("mc"),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(color: Colors.green),
+                      child: const Text("mc"),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(color: Colors.green),
+                      child: const Text("mc"),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(color: Colors.green),
+                      child: const Text("mc"),
+                    ),
+                    Container(
+                      decoration: const BoxDecoration(color: Colors.green),
+                      child: const Text("mc"),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
